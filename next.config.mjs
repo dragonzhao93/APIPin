ade6cfae55a58b6aceacccc6f7f75eb0ee8218fa
dragonzhao93 <zhaotianlong93@outlook.com>
2024-12-04 10:26:34 +0800

    feat: 重新路径

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*', // 前端请求的路径
+            destination: 'https://api.lolimi.cn/API/:path*', // 转发到的目标地址
+          },
+        ];
+      }
+};
 
 export default nextConfig;
